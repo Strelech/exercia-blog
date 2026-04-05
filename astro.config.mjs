ae362de3a1f4653db854daf871fe3128b0deab95
Strelech <33900122+Strelech@users.noreply.github.com>
2026-04-05 11:42:37 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://blog.exercia.org',
 });
+import sitemap from '@astrojs/sitemap';
+export default defineConfig({
+  site: 'https://blog.exercia.org',
+  integrations: [sitemap()],
+});
