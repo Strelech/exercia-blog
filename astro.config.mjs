b1ed63a6e1b66b847470523c015ccce00866ff02
Strelech <33900122+Strelech@users.noreply.github.com>
2026-07-19 15:35:57 +0200

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://blog.exercia.org',
   trailingSlash: 'always',
+  integrations: [sitemap()],
 });
